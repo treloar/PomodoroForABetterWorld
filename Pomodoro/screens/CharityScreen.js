ddfebda2a01028c3046bfa5918d02a77ea971b42
Richard Shu <richardjshu@gmail.com>
2018-09-30 01:50:56 -0400

    Assign a unique id to each charity box

--- Pomodoro/screens/CharityScreen.js
+++ Pomodoro/screens/CharityScreen.js
@@ -12,29 +12,33 @@ export class CharityScreen extends React.Component {
     super(props);
   }
 
+  selectCharity = (id) => {
+    alert("You have selected charity with id = " + id);
+    this.goToTimeScreen();
+  }
+
   goToTimeScreen = () => {
-    this.props.nav('time');   
+    this.props.nav('time');
   }
 
   render() {
     return (
       <View>
         <Text>Hello world</Text>
-        <View style={this.styles.charitycontainer}>
-          <CharityBox name="United Way Worldwide"/>
-          <CharityBox name="Task Force for Global Health"/>
-          <CharityBox name="Feeding America"/>
-          <CharityBox name="Salvation Army"/>
-          <CharityBox name="St. Jude Children's Research Hospital"/>
-          <CharityBox name="Habitat for Humanity International"/>
+        <View style={this.styles.charityContainer}>
+          <CharityBox id={1} onPress={this.selectCharity} name="United Way Worldwide"/>
+          <CharityBox id={2} onPress={this.selectCharity} name="Task Force for Global Health"/>
+          <CharityBox id={3} onPress={this.selectCharity} name="Feeding America"/>
+          <CharityBox id={4} onPress={this.selectCharity} name="Salvation Army"/>
+          <CharityBox id={5} onPress={this.selectCharity} name="St. Jude Children's Research Hospital"/>
+          <CharityBox id={6} onPress={this.selectCharity} name="Habitat for Humanity International"/>
         </View>
-        <Button onPress={this.goToTimeScreen} title="Select Charity"></Button>
       </View>
     );
   }
 
   styles = StyleSheet.create({
-    charitycontainer: {
+    charityContainer: {
       flexDirection: 'row',
       flexWrap: 'wrap',
       justifyContent: 'space-around',
