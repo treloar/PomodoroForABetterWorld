3750194eadc9ad95040120d863cfdf0f49819dad
James Treloar <james.treloar8@gmail.com>
2018-09-29 20:49:05 -0400

    skeleton

---
+++ Pomodoro/screens/PomodoroScreen.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import {
+  View,
+  Button,
+  Text,
+} from 'react-native';
+
+
+export class PomodoroScreen extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+              <View>
+              <Text> Pomodoro screen! </Text>
+              </View>
+    );
+  }
+}
+
