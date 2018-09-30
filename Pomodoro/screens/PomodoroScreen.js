06edded274328798be97f38186228a7f0faaa9f6
James Treloar <james.treloar8@gmail.com>
2018-09-30 02:43:33 -0400

    added video and colored background

--- Pomodoro/screens/PomodoroScreen.js
+++ Pomodoro/screens/PomodoroScreen.js
@@ -7,6 +7,7 @@ import {
     StyleSheet,
     Dimensions,
 } from 'react-native';
+import { Video } from 'expo';
 
 const styles = StyleSheet.create({
     tomatoContainer: {
@@ -15,13 +16,15 @@ const styles = StyleSheet.create({
         justifyContent: 'space-evenly',
     },
     tomatoImg: {
-        marginTop: 60,
-        marginBottom: 40,
         width: Dimensions.get('window').width * 2/3,
         height: Dimensions.get('window').width * 2/3,
     },
+    tomatoVid: {
+        width: Dimensions.get('window').width * 1.2,
+        height: Dimensions.get('window').width * 1.2,
+    },
     centralFlexContainer: {
-        paddingTop: 50,
+        display: 'flex',
         flexDirection: 'column',
         justifyContent: 'space-evenly',
     },
@@ -31,7 +34,9 @@ const styles = StyleSheet.create({
         fontWeight: '600',
     },
     ctaButton: {
-        paddingTop: 80,
+        marginBottom: 20,
+        marginLeft: Dimensions.get('window').width * 1/4,
+        width: Dimensions.get('window').width * 1/2,
     },
 });
 
@@ -68,19 +73,29 @@ export class PomodoroScreen extends React.Component {
     render() {
         let isFinished = this.state.time == 0;
         let buttons = (
-                <View>
-                    <Button style={styles.ctaButton} title="Change charity" onPress={this.pressGoToCharity}></Button>
-                    <Button style={styles.ctaButton} title="Change time" onPress={this.pressGoToTime}></Button>
+                <View style={{display: 'flex', flexDirection:'column'}}>
+                    <View style={styles.ctaButton}>
+                    <Button color="#FC152B" title="Change charity" onPress={this.pressGoToCharity}></Button>
+                    </View>
+                    <View style={styles.ctaButton}>
+                    <Button color="#FC152B" title="Change time" onPress={this.pressGoToTime}></Button>
+                    </View>
                 </View>
                 );
 
+        //<Image style={styles.tomatoImg} source={require('../img/tomato.png')}/>
+        //<Text style={styles.countdownText}>{ this.state.time }!</Text>
         return (
-                <View style={{flexDirection: 'row', justifyContent: 'space-evenly'}}>
+                <View style={{height: Dimensions.get('window').height, flexDirection: 'row', justifyContent: 'space-evenly', backgroundColor: '#FFCCFF'}}>
                     <View style={styles.centralFlexContainer}>
                         <View style={styles.tomatoContainer}>
-                            <Image style={styles.tomatoImg} source={require('../img/tomato.png')}/>
+							<Video
+							source={require('../vid/tomato10pink.mp4')}
+							style={styles.tomatoVid}
+                            shouldPlay
+							resizeMode={"cover"}
+							/>
                         </View>
-                        <Text style={styles.countdownText}>{ this.state.time }!</Text>
                         {isFinished &&
                             buttons
                         }
